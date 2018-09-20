504dcc3af5afedb7ffe76dc557395e9a6bcfa771
Simon Martinez <Simon@courtkingz.com>
2018-09-20 11:46:00 -0400

    add pdf suport

--- gemo.rb
+++ gemo.rb
@@ -1,13 +1,16 @@
 require 'countries'
+require './report'
 
-puts "WHat Country do you want a report on?"
+puts "What country do you want a report on?"
 
-country_name= gets.strip
+country_name = gets.strip
 
 country = ISO3166::Country.find_country_by_name(country_name)
 
 if country
-  puts "The flag for #{country.name} is #{country.emoji_flag}"
+  puts "The flag of #{country.name} is #{country.emoji_flag}"
+  Report.new(country).run
+  puts "For a full report, open country_report.pdf"
 else
   puts "Could not find a country called #{country_name}"
 end
